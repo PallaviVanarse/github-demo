a9804cecf02d0da192a24ca822775299c17e5697
Pallavi123 <pallavivanarse27@gmail.com>
2021-07-08 14:40:02 +0530

    Added fn implementationheaders for all the headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#add implementation
 def add(x,y):
     pass
+#Substract implementation
 def subtract(x,y):
     pass
+#Multiply implementation
 def multiply(x,y):
     pass
+#Divide implementation
 def divide(x,y):
-    pass
+	pass
